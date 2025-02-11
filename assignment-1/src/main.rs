a4c8c222cc6d05ed085edbf11a918726e6b9fc23
Nemya <yiduolnemya@gmail.com>
2025-02-11 12:25:52 -0500

    3.5 Control Flow exercise fibonacci sequence

--- assignment-1/src/main.rs
+++ assignment-1/src/main.rs
@@ -2,6 +2,8 @@ fn main() {
     temperature_converter(32, 'c');
     temperature_converter(32, 'f');
     temperature_converter(32, 'k');
+    let fib = nth_fibonacci(7);
+    println!("Your fib number is: {}", fib)
 }
 fn temperature_converter(temp: i32, unit: char) {
     if unit == 'C' || unit == 'c' {
@@ -13,4 +15,10 @@ fn temperature_converter(temp: i32, unit: char) {
     } else {
         println!("I don't know this unit.");
     }
+}
+fn nth_fibonacci(n: i32) -> i32 {
+    if n < 2 {
+        return n;
+    }
+    return nth_fibonacci(n - 1) + nth_fibonacci(n - 2);
 }
