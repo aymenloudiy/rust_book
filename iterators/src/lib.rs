29a43f85eb602b08591b332acf8a2a194df8458d
Nemya <yiduolnemya@gmail.com>
2025-02-26 12:13:22 -0500

    fix: Allow dead code

--- iterators/src/lib.rs
+++ iterators/src/lib.rs
@@ -1,9 +1,10 @@
 #[derive(PartialEq, Debug)]
+#[allow(dead_code)]
 struct Shoe {
     size: u32,
     style: String,
 }
-
+#[allow(dead_code)]
 fn shoes_in_size(shoes: Vec<Shoe>, shoe_size: u32) -> Vec<Shoe> {
     shoes.into_iter().filter(|s| s.size == shoe_size).collect()
 }
