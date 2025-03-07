f0a84e5cbaefa2e4f51d35f020f6c43e6b8fa895
Nemya <yiduolnemya@gmail.com>
2025-03-07 11:41:21 -0500

    fix: allow dead code

--- weak_T/src/main.rs
+++ weak_T/src/main.rs
@@ -3,13 +3,14 @@ use std::cell::RefCell;
 use std::rc::{Rc, Weak};
 
 #[derive(Debug)]
+#[allow(dead_code)]
 struct Node {
     value: i32,
     parent: RefCell<Weak<Node>>,
     children: RefCell<Vec<Rc<Node>>>,
 }
 #[derive(Debug)]
-
+#[allow(dead_code)]
 enum List {
     Cons(i32, RefCell<Rc<List>>),
     Nil,
