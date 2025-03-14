91f504444906f051924e9fe5a7b48e528210de91
Nimuel <yiduolnemya@gmail.com>
2025-03-14 10:31:22 -0400

    19.2: Refutability

---
+++ refutability/src/main.rs
@@ -0,0 +1,9 @@
+fn main() {
+    if let Some(x) = Some(50) {
+        println!("{x}");
+    }
+    // let Some(x) = some_option_value;
+    // if let x = 5 {
+    //     println!("{x}");
+    // };
+}
